68db734bec203aa3ef966b23bd887f5766a61b9a
navidmostafiz <navidmostafiz@gmail.com>
2020-02-21 02:39:28 -0500

    Initial Commit

---
+++ NMUIDeviceAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NMUIDeviceAPP
+//
+//  Created by Navid on 2/21/20.
+//  Copyright © 2020 UnitedStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
